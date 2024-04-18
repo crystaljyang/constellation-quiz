b3a694837b01bc231a3836e1f6a563d3d1a25f7d
Crystal Yang <cy2577@barnard.edu>
2024-04-17 22:47:23 -0400

    updated constellation info

--- server.py
+++ server.py
@@ -5,7 +5,6 @@ app = Flask(__name__)
 
 popular_constellations = [
     {
-        'id': 1,
         'name': 'Ursa Major (Big Dipper)',
         'season': 'Spring',
         'hemisphere': 'Northern',
@@ -15,19 +14,17 @@ popular_constellations = [
         'image': './media/big-dipper-ss.png',
         'namesake': 'The Big Dipper is named after its resemblance to a large ladle or dipper. The word "dipper" comes from the Old English word "dyppan," which means "to dip." The word "ladle" comes from the Old English word "hlædel," which means "a vessel for drawing out liquid." The Big Dipper is also known as the Plough in the United Kingdom and Ireland.',               
     },
-        {
-        'id': 2,
-        'name': 'Ursa Minor (Small Dipper)',
+    {
+        'name': 'Ursa Minor (Little Dipper)',
         'season': 'Spring',
         'hemisphere': 'Northern',
-        'number of stars': '19',
-        'brightest star': 'Alioth',
-        'characteristics': 'info about small dipper',
-        'image': './media/big-dipper-ss.png',
-        'namesake': 'info about name',               
+        'number of stars': '7',
+        'brightest star': 'Polaris',
+        'characteristics': 'The Little Dipper is a circumpolar constellation, meaning it is visible year-round in the Northern Hemisphere. It is part of the Ursa Minor constellation, which is the 56th largest constellation in the sky. The Little Dipper is made up of seven bright stars that form a shape that looks like a small ladle or dipper. The two stars that form the front of the ladle are called the Guardians because they guard the North Star, Polaris. The Little Dipper is also known as the Little Bear in Latin.',
+        'image': './media/little-dipper-ss.png',
+        'namesake': 'The Little Dipper is named after its resemblance to a small ladle or dipper. The word "dipper" comes from the Old English word "dyppan," which means "to dip." The word "ladle" comes from the Old English word "hlædel," which means "a vessel for drawing out liquid." The Little Dipper is also known as the Little Bear in Latin.',
     },
     {
-        'id': 3,
         'name': 'Cygnus (Northern Cross)',
         'season': 'Summer',
         'hemisphere': 'Northern',
@@ -38,7 +35,6 @@ popular_constellations = [
         'namesake': 'Cygnus is named after the Latin word for swan. In Greek mythology, Cygnus is associated with several myths, including the story of Zeus and Leda. According to legend, Zeus transformed himself into a swan to seduce Leda, the queen of Sparta. The union between Zeus and Leda produced several children, including Helen of Troy and the twins Castor and Pollux. Cygnus is also associated with the story of Orpheus, a legendary musician who was transformed into a swan after his death.',
     },
     {
-        'id': 4,
         'name': 'Cassiopeia',
         'season': 'Fall',
         'hemisphere': 'Northern',
@@ -49,15 +45,14 @@ popular_constellations = [
         'namesake': 'Cassiopeia is named after the queen of Aethiopia in Greek mythology. According to legend, Cassiopeia was the wife of King Cepheus and the mother of Princess Andromeda. She was known for her beauty and vanity, which ultimately led to her downfall. Cassiopeia was placed in the sky as a punishment for her arrogance, where she is forced to circle the North Star for eternity. The constellation is also known as the "Celestial W" because of its distinctive shape.',
     },
     {
-       'id': 5,
         'name': 'Orion',
-        'season': 'Spring',
+        'season': 'Winter',
         'hemisphere': 'Northern',
-        'number of stars': '19',
-        'brightest star': 'Alioth',
-        'characteristics': 'info',
-        'image': './media/big-dipper-ss.png',
-        'namesake': 'info',               
+        'number of stars': '7',
+        'brightest star': 'Rigel',
+        'characteristics': 'Orion is one of the most recognizable constellations in the sky. It is named after the mythological hunter Orion, who is often depicted holding a bow and arrow. Orion is best seen in the winter months in the Northern Hemisphere and is known for its distinctive shape, which resembles a hunter with a belt and sword. The constellation is home to several bright stars, including Betelgeuse and Rigel. Orion is part of the Orion family of constellations and is located near the Taurus and Gemini constellations.',
+        'image': './media/orion-ss.png',
+        'namesake': 'Orion is named after the mythological hunter Orion, who is often depicted holding a bow and arrow. In Greek mythology, Orion was a skilled hunter who was placed in the sky by the goddess Artemis after his death. The constellation is best seen in the winter months in the Northern Hemisphere and is known for its distinctive shape, which resembles a hunter with a belt and sword. Orion is also associated with several myths and legends from various cultures around the world.',
     },
 ]
 
@@ -80,17 +75,6 @@ def search():
 @app.route('/hello')
 def hello():
    return render_template('welcome.html')
-
-# VIEW
-@app.route('/view/<int:id>')
-def view_item(id):
-    item = next((item for item in popular_constellations if item['id'] == id), None)
-    if item:
-        return render_template('view.html', item=item)
-    else:
-        return "Item not found", 404
-
-
 
 if __name__ == '__main__':
    app.run(debug = True)
